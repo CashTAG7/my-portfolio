3de584ca595f166dcce7a8b9daabf2ef72e46034
Md. Noman Ebeny Shahid <nomanebeny@gmail.com>
2023-11-03 12:12:41 +0600

    updated resume link

--- src/app/about/ResumeButton.tsx
+++ src/app/about/ResumeButton.tsx
@@ -34,7 +34,7 @@ const ResumeButton = () => {
       <Magnet>
         <Link
           href={
-            "https://drive.google.com/file/d/16ofQgwxPMWuY5CV_7Y8D3WMyjb-rkc5X/view?usp=drive_link"
+            "https://drive.google.com/file/d/1X3DgVQi3rAAJ2YczIu8Tl2rQaUVy3NEC/view?usp=drive_link"
           }
           download={"Resume.pdf"}
           className="btn btn-neutral mx-4 sm:mx-8 md:mx-0 lg:mx-16 xl:mx-20 2xl:mx-24 mb-8 md:my-8 bg-dark-gray dark:bg-lighter-gray text-lighter-gray dark:text-dark-gray hover:bg-lighter-gray dark:hover:bg-dark-gray hover:text-dark-gray font-medium dark:hover:text-lighter-gray dark:hover:border-lighter-gray"
